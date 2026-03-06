047434ce4969453f3712013160322999cf4d3691
minguh <aiguh97@gmail.com>
2026-03-06 10:32:38 +0700

    fixing ssl pinning dan ci

--- lib/core/ssl_pinning.dart
+++ lib/core/ssl_pinning.dart
@@ -2,25 +2,27 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 
 class MyHttpOverrides extends HttpOverrides {
-  final SecurityContext _securityContext;
+  final SecurityContext securityContext;
 
-  MyHttpOverrides(this._securityContext);
+  MyHttpOverrides(this.securityContext);
 
   static Future<void> initialize() async {
     try {
       final certData = await rootBundle.load('assets/certs/cert.pem');
       final bytes = certData.buffer.asUint8List();
-      final sc = SecurityContext(withTrustedRoots: true);
-      sc.setTrustedCertificatesBytes(bytes);
-      HttpOverrides.global = MyHttpOverrides(sc);
+
+      final context = SecurityContext(withTrustedRoots: true);
+      context.setTrustedCertificatesBytes(bytes);
+
+      HttpOverrides.global = MyHttpOverrides(context);
     } catch (e) {
-      // If cert not provided, we don't set overrides. App should still run in dev.
-      // Keep this silent; caller may log if desired.
+      // Fail silently for development
+      print('SSL Pinning not initialized: $e');
     }
   }
 
   @override
   HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(_securityContext);
+    return HttpClient(context: securityContext);
   }
 }
